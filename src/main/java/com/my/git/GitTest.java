852153874b00c857e6815e53d1d70549dc583acf
SimpleCriticism <84172640+SimpleCriticism@users.noreply.github.com>
2021-05-14 18:30:03 +0800

    pull commit01

--- src/main/java/com/my/git/GitTest.java
+++ src/main/java/com/my/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello ttxx!");
         System.out.println("push!");
         System.out.println("push121!");
+        System.out.println("pushs!");
     }
 }
