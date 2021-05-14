89ebf2550ed9e80d1894ffbe4b976c784ab11ed2
mygitxcz <mygitxcz@163.com>
2021-05-14 17:59:10 +0800

    push commit01

--- src/main/java/com/my/git/GitTest.java
+++ src/main/java/com/my/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("hello mastr");
         System.out.println("hello ttxx!");
+        System.out.println("push!");
     }
 }
