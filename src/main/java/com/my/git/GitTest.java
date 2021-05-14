6bf5b01cc3040ee25e1bc705be39dfbff972a343
mygitxcz <mygitxcz@163.com>
2021-05-14 17:36:06 +0800

    second commit

--- src/main/java/com/my/git/GitTest.java
+++ src/main/java/com/my/git/GitTest.java
@@ -3,5 +3,7 @@ package com.my.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
